4f4cf084cac8b26ac38c52fc5f35c1511280ab75
richardabuan <richard.abuan@gmail.com>
2025-02-06 03:24:32 +0800

    Updated files

--- __init__.py
+++ __init__.py
@@ -1,25 +1,39 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+from palindrome_mhartl.phrase import Phrase
 
 
 def create_app(test_config=None):
     """Create and configure the app."""
     app = Flask(__name__, instance_relative_config=True)
-    
-    
-    
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing.
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # Load the test config if passed in.
+        app.config.from_mapping(test_config)
+
+    # Ensure the instance folder exists.
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
     @app.route("/")
     def index():
-        return render_template("index.html")
+        return render_template("index.html", page_title="Home")
 
     @app.route("/about")
     def about():
-        return render_template("about.html")
+        return render_template("about.html", page_title="About")
 
     @app.route("/palindrome")
     def palindrome():
-        return render_template("palindrome.html")
+        return render_template("palindrome.html",
+                               page_title="Palindrome Detector")
 
     return app
 
